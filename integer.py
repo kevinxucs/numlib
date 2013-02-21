912c120bfc55394cbd89f1ba00a3a62114d40cc5
Kaiwen Xu <xu227@purdue.edu>
2013-02-21 01:41:55 -0500

    add IntegerNumber generator to integer module and updated isInt method.
    IntegerNumber generator now generates infinitely many integer numbers without stop.
    isInt now checks the type of input value.
    add isOdd method, which is just the opposite of isEven.

--- integer.py
+++ integer.py
@@ -3,8 +3,40 @@
 # 
 # module for calculating integers
 
+# Integer generator (non-stop)
+class IntegerNumber:
+	def __init__(self, start=0):
+		"""'start' - the number start from. 0 by default.
+		"""
+		if isInt(start):
+			self.gen = start
+			self.start = start
+		else:
+			raise ValueError("Start number must be an integer.") 
+
+	def __iter__(self):
+		return self
+
+	def next(self):
+		self.gen += 1
+		return (self.gen - 1)
+
+	def reset(self, start=None):
+		if start == None:
+			self.gen = self.start
+		else:
+			if isInt(start):
+				self.gen = start
+				self.start = start
+			else:
+				raise ValueError("Start number must be an integer.") 
+
+
 def isInt(f):
-	i = int(f)
+	try:
+		i = int(f)
+	except:
+		return False
 	if i == f:
 		return True
 	else:
@@ -21,5 +53,8 @@ def isEven(n):
 		return True
 	else:
 		return False
+
+def isOdd(n):
+	return not isEven(n)
 
 getBinStr = lambda x: x > 0 and str(bin(x))[2:] or "-" + str(bin(x))[3:]
