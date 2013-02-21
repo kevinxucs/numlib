d54f726b335cfbfdc627572f5d1ec7b7c90e3dcc
Kaiwen Xu <xu227@purdue.edu>
2013-02-21 02:01:07 -0500

    add EvenNumber generator and OddNumber generator to integer module. minor comment updates.

--- integer.py
+++ integer.py
@@ -6,7 +6,7 @@
 # Integer generator (non-stop)
 class IntegerNumber:
 	def __init__(self, start=0):
-		"""'start' - the number start from. 0 by default.
+		"""'start' - the number starts from. 0 by default.
 		"""
 		if isInt(start):
 			self.gen = start
@@ -18,10 +18,15 @@ class IntegerNumber:
 		return self
 
 	def next(self):
+		"""Get next integer.
+		"""
 		self.gen += 1
 		return (self.gen - 1)
 
 	def reset(self, start=None):
+		"""Reset generator.
+		'start' - if not set, reset to the start number which initialized with.
+		"""
 		if start == None:
 			self.gen = self.start
 		else:
@@ -30,6 +35,24 @@ class IntegerNumber:
 				self.start = start
 			else:
 				raise ValueError("Start number must be an integer.") 
+
+class EvenNumber(IntegerNumber):
+	def next(self):
+		"""Get next even integer.
+		"""
+		self.gen += 2
+		if not isEven(self.gen):
+			self.gen += 1
+		return (self.gen - 2)
+
+class OddNumber(IntegerNumber):
+	def next(self):
+		"""Get next odd integer.
+		"""
+		self.gen += 2
+		if not isOdd(self.gen):
+			self.gen += 1
+		return (self.gen - 2)
 
 def isInt(f):
 	try:
