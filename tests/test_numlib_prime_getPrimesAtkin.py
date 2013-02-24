46a91140786fdaa99b891176285d25820ab12991
Kaiwen Xu <xu227@purdue.edu>
2013-02-24 02:49:01 -0500

    add test cases in getPrimesAtkin for testing run time of empty loops.

--- tests/test_numlib_prime_getPrimesAtkin.py
+++ tests/test_numlib_prime_getPrimesAtkin.py
@@ -1,5 +1,22 @@
+import math
 from time import clock
 from numlib.prime import getPrimesAtkin
+
+def testEmpty(below):
+	st = clock()
+	print 'Test Empty Loop below', below
+	listlen = below + 1
+	mask = listlen * [False]
+	mask[2] = True
+	mask[3] = True
+	sqrtlimit = int(math.sqrt(below)) + 1
+	for x in xrange(1, sqrtlimit):
+		for y in xrange(1, sqrtlimit):
+			pass
+	for n in xrange(5, sqrtlimit):
+		pass
+	print 'Total Time:', clock() - st
+
 
 def testNonTread(below):
 	st = clock()
@@ -8,6 +25,7 @@ def testNonTread(below):
 	print 'Total Time: ' + str(clock() - st) + 's'
 	return result
 
+
 def testThread(below):
 	st = clock()
 	print 'Testing Thread below ' + repr(below)
@@ -15,7 +33,9 @@ def testThread(below):
 	print 'Total Time: ' + str(clock() - st) + 's'
 	return result
 
+
 def test(below):
+	testEmpty(below)
 	r1 = testNonTread(below)
 	r2 = testThread(below)
 	if r1 == r2:
@@ -23,5 +43,7 @@ def test(below):
 	else:
 		print 'different'
 
-test(2000000)
-test(20000000)
+
+if __name__ == '__main__':
+	test(2000000)
+	test(20000000)
